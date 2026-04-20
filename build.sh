40f71af9d67edab37f37b6f2a67a3aa44f953374
Ahmad000191 <Star.legend00101@gmail.com>
2026-04-19 20:46:05 -0400

    force update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 pip install -r requirements.txt
 
-curl -L https://stockfishchess.org/files/stockfish_16.1_linux_x64_avx2.zip -o stockfish.zip
+apt-get update && apt-get install -y unzip
+
+curl -L https://github.com/official-stockfish/Stockfish/releases/latest/download/stockfish-ubuntu-x86-64-avx2.zip -o stockfish.zip
 
 unzip stockfish.zip
+
 mkdir -p stockfish
 mv stockfish*/stockfish* stockfish/stockfish
 
