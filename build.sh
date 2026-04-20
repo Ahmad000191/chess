e1cd920be830043af6e27c60015339c4f99e6271
Ahmad000191 <Star.legend00101@gmail.com>
2026-04-19 21:06:41 -0400

    use correct linux stockfish binary

--- build.sh
+++ build.sh
@@ -1,10 +1,15 @@
 pip install -r requirements.txt
 
-# make folder
 mkdir -p stockfish
 
-# download raw binary (NOT zip)
-curl -L https://github.com/official-stockfish/Stockfish/releases/download/sf_16/stockfish-ubuntu-x86-64-avx2 -o stockfish/stockfish
+# download guaranteed working Linux binary
+curl -L https://stockfishchess.org/files/stockfish_16_linux_x64.zip -o stockfish.zip
+
+# extract using python
+python -m zipfile -e stockfish.zip stockfish
+
+# move correct binary (path inside zip)
+mv stockfish/stockfish_16_linux_x64/stockfish_16_x64 stockfish/stockfish
 
 # make executable
 chmod +x stockfish/stockfish
