1ecbb445b17bee96a7478ab5af50f012db6a678b
Ahmad000191 <Star.legend00101@gmail.com>
2026-04-19 21:11:01 -0400

    use system stockfish

--- build.sh
+++ build.sh
@@ -1,15 +1,4 @@
 pip install -r requirements.txt
 
-mkdir -p stockfish
-
-# download guaranteed working Linux binary
-curl -L https://stockfishchess.org/files/stockfish_16_linux_x64.zip -o stockfish.zip
-
-# extract using python
-python -m zipfile -e stockfish.zip stockfish
-
-# move correct binary (path inside zip)
-mv stockfish/stockfish_16_linux_x64/stockfish_16_x64 stockfish/stockfish
-
-# make executable
-chmod +x stockfish/stockfish
+apt-get update
+apt-get install -y stockfish
